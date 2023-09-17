4f84fe442163193737841c03cd6933eb8b1e0f29
Iriza-peace <irizapeace.com>
2023-09-17 20:34:34 +0200

    added other changes to be deployed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Project1 from '../assets/cover.png'
+import Project2 from '../assets/project2.png'
+import Project3 from '../assets/project3.png'
 function projects() {
     return (
         <>
@@ -38,19 +40,19 @@ function projects() {
 >
   <img
     className="object-cover h-64 md:w-72 md:h-full md:rounded-none md:rounded-l-lg"
-    src={Project1}
+    src={Project2}
     alt=""
   />
   <div className="flex flex-col justify-between p-4 leading-normal ">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      FOOD HUB
+      CORRASET
      
     </h5>
     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
     I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.
     </p>
     <p className='mt-6 '>
-    <a href="#" class="text-white hover:bg-yellow-400 hover:text-white ring-2 ring-yellow-400 font-bold rounded-full text-md p-4 mr-4 mb-2 ">VIEW PROJECTS</a>
+    <a href="https://www.behance.net/gallery/159442947/corasset" class="text-white hover:bg-yellow-400 hover:text-white ring-2 ring-yellow-400 font-bold rounded-full text-md p-4 mr-4 mb-2 ">VIEW PROJECTS</a>
     </p>
   </div>
 </a>
@@ -64,19 +66,19 @@ function projects() {
   >
     <img
       className="object-cover h-64 md:w-72 md:h-full md:rounded-none md:rounded-l-lg"
-      src={Project1}
+      src={Project3}
       alt=""
     />
     <div className="flex flex-col justify-between p-4 leading-normal ">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        FOOD HUB
+        TENZI
        
       </h5>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
       I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.
       </p>
       <p className='mt-6 '>
-      <a href="#" class="text-white hover:bg-yellow-400 hover:text-white ring-2 ring-yellow-400 font-bold rounded-full text-md p-4 mr-4 mb-2 ">VIEW PROJECTS</a>
+      <a href="https://www.behance.net/gallery/156176601/TENZI-a-question-answer-mobile-application" class="text-white hover:bg-yellow-400 hover:text-white ring-2 ring-yellow-400 font-bold rounded-full text-md p-4 mr-4 mb-2 ">VIEW PROJECTS</a>
       </p>
     </div>
   </a>
